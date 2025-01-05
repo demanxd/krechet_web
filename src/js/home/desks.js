a1a6ab109d24507d41d2c57a9483cfe0e05efa2d
DVSh <dvsh36@yandex.ru>
2025-01-05 14:14:33 +0300

    Added basic user possibility for creation and very simpe manipulation with tasks

--- src/js/home/desks.js
+++ src/js/home/desks.js
@@ -1,18 +1,67 @@
 import React from "react"
 import { Link } from "react-router-dom";
+import { useRef, useState, useEffect, useContext } from 'react';
+import axios from '../api/axios';
+import useAuth from '../context/UseAuth';
+
+const BOARD_DELETE = "/board/delete"
 
 
 
 const Desks = ({desk}) => {
+    const {auth} = useAuth();
+    const [errMsg, setErrMsg] = useState('');
+    
+    const deleteItem = async (e) => {
+        e.preventDefault();
+        console.log("deleting");
+        console.log("deleting desk = ", desk);
+
+        try {
+            const response = await axios.post(BOARD_DELETE,
+                {
+                    "boardID": desk.id,
+                },
+                {
+                    timeout: 3000,
+                    headers: {
+                        'Authorization': 'Bearer ' + auth.accessToken,
+                        'Host' : 'Krechet UI'
+                    }
+                }
+            );
+            console.log("console return", JSON.stringify(response?.data));
+            alert('Доска удалена!');
+        } catch (err) {
+            if (!err?.response) {
+                console.log('No Server Response');
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 400) {
+                console.log('Missing Username or Password');
+                setErrMsg('Missing Username or Password');
+            } else if (err.response?.status === 401) {
+                console.log('Unauthorized');
+                setErrMsg('Unauthorized');
+            } else {
+                console.log('Login Failed');
+                setErrMsg('Login Failed');
+            }
+            alert(err.response?.data);
+        }
+
+    }
     
     console.log("Desks, desk", desk);
     return (
         <div>
         <div className="desk_small">
             <div className="desk_small_name">
-            <Link key={desk.id} to = {`/desk/${desk.id}`}>
-                <h3>{desk.name}</h3>
-            </Link>
+                <Link key={desk.id} to = {`/desk/${desk.id}`}>
+                    <h3>{desk.name}</h3>
+                </Link>
+                <button className="delete-button" onClick={(e) => deleteItem(e)} >
+                    <img src="{DELETE_ICON_PATH}" alt="Удалить" width="30" height="30" />
+                </button>
             </div>
             <div className="desk_small_desc">
             <p>{desk.description}</p>
--- src/js/home/home.js
+++ src/js/home/home.js
@@ -3,20 +3,30 @@ import AuthContext from "../context/AuthProvider";
 import React from "react";
 import DesksList from "./deskslist"
 import useAuth from '../context/UseAuth';
+import {useNavigate} from 'react-router-dom';
 
 import axios from '../api/axios';
-const DESKS_URL = '/board/all';
+import Button from '../common/button'
 
+const DESKS_URL = '/board/all';
+const DESK_CREATE_URL = '/board/create'
+const DESK_REMOVE_URL = '/board/delete'
+const DESK_UPDATE_URL = '/board/update'
+
+
+//add KNOPA4KI
 
 const Home = () => {
     const [errMsg, setErrMsg] = useState('');
     const [desks, setDesks] = useState([]);
     const componentIsMounted = useRef(true);
+    const errRef = useRef();
     let err;
     console.log("Home");
     const {auth} = useAuth();
     console.log("Home");
     console.log("Home auth = ", auth);
+    const navigate = useNavigate();
     useEffect(() => {
 
         async function fetchDesks() {
@@ -51,6 +61,43 @@ const Home = () => {
 
         fetchDesks();
     },[]);
+    const [deskname, setDeskname] = useState('');
+
+    const handleCreateDesk = async (e) => {
+        e.preventDefault();
+        console.log("deskname ", deskname)
+
+        try {
+            const response = await axios.post(DESK_CREATE_URL,
+                {
+                    'boardname': deskname,
+                },
+                {
+                    timeout: 3000,
+                    headers: {
+                        'Authorization': 'Bearer ' + auth.accessToken,
+                        'Host' : 'Krechet UI'
+                    }
+                }
+            );
+            console.log(JSON.stringify(response?.data));
+            // navigate('/home');
+        } catch (err) {
+            if (!err?.response) {
+                console.log('No Server Response');
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 400) {
+                console.log('Missing Username or Password');
+                setErrMsg('Missing Username or Password');
+            } else if (err.response?.status === 401) {
+                console.log('Unauthorized');
+                setErrMsg('Unauthorized');
+            } else {
+                console.log('Login Failed');
+                setErrMsg('Login Failed');
+            }
+        }
+    }
     
     console.log("Home componentIsMounted = ", componentIsMounted);
     
@@ -60,15 +107,24 @@ const Home = () => {
     return (
         <div>
             { <div className="home">
-                <div className="tasks_body">
-                {
-                    desks.map( (element) => ( 
-                            <div key = {element.id} >
-                                <DesksList desk={element} />
-                            </div>
+                <div className="desks_body">
+                    {
+                        desks.map( (element) => ( 
+                                <div key = {element.id} >
+                                    <DesksList desk={element} />
+                                </div>
+                            )
                         )
-                    )
-                }
+                    }
+                    <form onSubmit={(e) => handleCreateDesk(e)} className="desks_body" name="dsfa">
+                        <input className="desks_body"
+                            type="text"
+                            onChange={(e) => setDeskname(e.target.value)}
+                            value = {deskname}
+                            required
+                        />
+                        <button className="desks_body">Add desk</button>
+                    </form>
                 </div>
                 <div className="top_panel">
                     <h3>top panel</h3>
